42aa3c5b2681c5e6473b964617128d4ab72a572b
iitdwtf <148150113+iitdwtf@users.noreply.github.com>
2023-10-17 04:02:40 +0530

    changes in public folder

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
         <p>
           Tumhari agar chaggi h aur tumhari placement nai lag rhi h
-        </p>
-        <p>
+          <br />
           then you are at the right place.
         </p>
+
       </header>
-    </div>
+    </main>
   );
 }
 
